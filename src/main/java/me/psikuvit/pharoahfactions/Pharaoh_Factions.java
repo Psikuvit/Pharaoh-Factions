fbe09617feb92aa20930d5d15e07586476264a6a
Psikuvit <username@whatever.com>
2023-05-06 23:08:41 +0200

    test

---
+++ src/main/java/me/psikuvit/pharoahfactions/Pharaoh_Factions.java
@@ -0,0 +1,17 @@
+package me.psikuvit.pharoahfactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pharaoh_Factions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
