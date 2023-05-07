132209c9eb6b5c2697f27ce1b13482d4cc43e032
Psikuvit <username@whatever.com>
2023-05-07 19:10:27 +0200

    Added hex coloring support

--- src/main/java/me/psikuvit/pharoahfactions/Utils/Messages.java
+++ src/main/java/me/psikuvit/pharoahfactions/Utils/Messages.java
@@ -1,17 +1,28 @@
 package me.psikuvit.pharoahfactions.Utils;
 
+
 import org.bukkit.ChatColor;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Messages {
 
-    public static String color(String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
-
-    }
     public static List<String> color(List<String> msg) {
         return msg.stream().map(Messages::color).collect(Collectors.toList());
+    }
+
+    public static String color(String message) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String convertedCode = hexCode.replaceAll("#", "x").replaceAll("(?<=x)(.)", "&$1");
+            message = message.replace(hexCode, convertedCode);
+            matcher = pattern.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
